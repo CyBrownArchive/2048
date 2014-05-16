31d5fee29dc5d2bbba21936462f64969cf789a39
Cy Brown <cy@cybrown.fr>
2014-05-16 20:55:21 +0200

    Mise à jour fake local storage

--- js/fake_storage_manager.js
+++ js/fake_storage_manager.js
@@ -2,19 +2,18 @@ function FakeStorageManager() {
 }
 
 FakeStorageManager.prototype.localStorageSupported = function () {
-
 };
 
 FakeStorageManager.prototype.getBestScore = function () {
 };
 
-FakeStorageManager.prototype.setBestScore = function (score) {
+FakeStorageManager.prototype.setBestScore = function () {
 };
 
 FakeStorageManager.prototype.getGameState = function () {
 };
 
-FakeStorageManager.prototype.setGameState = function (gameState) {
+FakeStorageManager.prototype.setGameState = function () {
 };
 
 FakeStorageManager.prototype.clearGameState = function () {
