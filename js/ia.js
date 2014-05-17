cda6313d9c134e70e5ada013a6bc635bfb959965
Cy Brown <cy@cybrown.fr>
2014-05-17 14:20:39 +0200

    Ajout de boutons pour plus d'outils, refactorisation du runner en prototype

--- js/ia.js
+++ js/ia.js
@@ -98,7 +98,9 @@ var grid_score = function (grid) {
 };
 
 window.onload = function () {
-    var btn = document.getElementById("cy-test").addEventListener('click', function (event) {
+    gridStack = [];
+    gridCur = 0;
+    var btn = document.getElementById('btn-start').addEventListener('click', function (event) {
         var _over;
         Object.defineProperty(game, 'over', {
             get: function () {
@@ -109,57 +111,103 @@ window.onload = function () {
                 document.body.dispatchEvent(new Event('game.over'));
             }
         });
-        switchStrategy(LeftUpLoopStrategy);
-        runner();
+        runner.switchStrategy(LeftUpLoopStrategy);
+        runner.start();
+    });
+    document.getElementById("btn-pause").addEventListener('click', function (event) {
+        runner.pause();
+    });
+    document.getElementById("btn-one").addEventListener('click', function (event) {
+        runner.one();
+    });
+    var oldGrid = null;
+    document.getElementById("btn-save").addEventListener('click', function (event) {
+        oldGrid = game.grid.serialize();
+    });
+    document.getElementById("btn-restore").addEventListener('click', function (event) {
+        game.grid = new Grid(4, oldGrid.cells);
+        game.actuate();
     });
 };
 
-var currentStrategy = null;
-var strategyStack = [];
-var sleepTime = 100;
-var runner = function () {
-    var inter = setInterval(function () {
-        var oldGridState = JSON.stringify(game.grid.cells);
-        currentStrategy.do();
-        var gridState = JSON.stringify(game.grid.cells);
-        if (oldGridState == gridState) {
-            currentStrategy.onSame();
-        }
-    }, sleepTime);
-    var onGameOver = function (event) {
-        clearInterval(inter);
-        document.body.removeEventListener('game.over', onGameOver);
-    }
-    document.body.addEventListener('game.over', onGameOver);
+var IaRunner = function () {
+    this.currentStrategy = null;
+    this.strategyStack = [];
+    this.sleepTime = 50;
+    this.inter = null;
+    this.started = false;
 };
 
+IaRunner.prototype.run = function () {
+    var oldGridState = JSON.stringify(game.grid.cells);
+    currentStrategy.do();
+    var gridState = JSON.stringify(game.grid.cells);
+    if (oldGridState == gridState) {
+        currentStrategy.onSame();
+    }
+};
 
-var switchStrategy = function (strategy) {
+IaRunner.prototype.start = function () {
+    if (this.started) {
+        this.resume();
+    } else {
+        var _this = this;
+        this.started = true;
+        this.inter = setInterval(this.run, this.sleepTime);
+        var onGameOver = function (event) {
+            _this.stop();
+            document.body.removeEventListener('game.over', onGameOver);
+        }
+        document.body.addEventListener('game.over', onGameOver);
+    }
+};
+
+IaRunner.prototype.one = function () {
+    this.run();
+};
+
+IaRunner.prototype.resume = function () {
+    this.inter = setInterval(this.run, this.sleepTime);
+};
+
+IaRunner.prototype.pause = function () {
+    clearInterval(this.inter);
+};
+
+IaRunner.prototype.stop = function () {
+    clearInterval(this.inter);
+    this.started = false;
+};
+
+IaRunner.prototype.switchStrategy = function (strategy) {
     currentStrategy = new strategy();
     console.log('Switching to ' + currentStrategy.name);
 };
 
-var callStrategy = function (strategy, callId) {
+IaRunner.prototype.callStrategy = function (strategy, callId) {
     var strat = new strategy();
     console.log('Calling ' + strat.name);
-    strategyStack.push({
+    this.strategyStack.push({
         strategy: currentStrategy,
         callId: callId
     });
     currentStrategy = strat;
 };
 
-var returnStrategy = function () {
-    if (!strategyStack.length) {
+IaRunner.prototype.returnStrategy = function () {
+    if (!this.strategyStack.length) {
         throw new Error('No more strategy to pop');
     }
-    var retStrategy = strategyStack[strategyStack.length - 1].strategy;
-    var callId = strategyStack[strategyStack.length - 1].callId;
+    var retStrategy = this.strategyStack[this.strategyStack.length - 1].strategy;
+    var callId = this.strategyStack[this.strategyStack.length - 1].callId;
     currentStrategy = retStrategy;
-    strategyStack.pop();
+    this.strategyStack.pop();
     console.log('Return ' + currentStrategy.name);
     currentStrategy.onReturn(callId);
 };
+
+
+var runner = new IaRunner();
 
 var Strategy = function () {
 
@@ -195,11 +243,17 @@ LeftUpLoopStrategy.prototype.do = function () {
     } else if (this.mvt == UP) {
         this.mvt = LEFT;
     }
+
+    //var best = best_move([UP, LEFT], function (grid) {
+    //    //return free_tiles(grid);
+    //    return grid_score(grid);
+    //});
+    //game.inputManager.emit('move', best);
 };
 
 LeftUpLoopStrategy.prototype.onSame = function () {
     this.lastGridState = JSON.stringify(game.grid.cells);
-    callStrategy(RightThenUpLoopThenLeftStrategy, 1);
+    runner.callStrategy(RightThenUpLoopThenLeftStrategy, 1);
 };
 
 LeftUpLoopStrategy.prototype.onReturn = function (callId) {
@@ -207,7 +261,7 @@ LeftUpLoopStrategy.prototype.onReturn = function (callId) {
         case 1:
             var gridState = JSON.stringify(game.grid.cells);
             if (this.lastGridState == gridState) {
-                callStrategy(DownUpStrategy, 2);
+                runner.callStrategy(DownUpStrategy, 2);
             }
             break;
         case 2:
@@ -230,16 +284,16 @@ RightThenUpLoopThenLeftStrategy.prototype = Object.create(Strategy.prototype);
 RightThenUpLoopThenLeftStrategy.prototype.name = 'RightThenUpLoopThenLeftStrategy';
 
 RightThenUpLoopThenLeftStrategy.prototype.do = function () {
-    callStrategy(this.strategies[this.cur], 1);
+    runner.callStrategy(this.strategies[this.cur], 1);
 };
 
 RightThenUpLoopThenLeftStrategy.prototype.onReturn = function (callId) {
     this.cur++;
     if (this.cur == this.strategies.length) {
-        returnStrategy();
+        runner.returnStrategy();
         return;
     } else {
-        callStrategy(this.strategies[this.cur]);
+        runner.callStrategy(this.strategies[this.cur]);
     }
 };
 
@@ -253,7 +307,7 @@ RightStrategy.prototype.name = 'RightStrategy';
 
 RightStrategy.prototype.do = function () {
     game.inputManager.emit('move', RIGHT);
-    returnStrategy();
+    runner.returnStrategy();
 };
 
 RightStrategy.prototype.onSame = function () {
@@ -270,11 +324,15 @@ UpLoopStrategy.prototype = Object.create(Strategy.prototype);
 UpLoopStrategy.prototype.name = 'UpLoopStrategy';
 
 UpLoopStrategy.prototype.do = function () {
-    game.inputManager.emit('move', UP);
+    if (getLine(game.grid, 0)[0] == 0) {
+        runner.returnStrategy();
+    } else {
+        game.inputManager.emit('move', UP);
+    }
 };
 
 UpLoopStrategy.prototype.onSame = function () {
-    returnStrategy();
+    runner.returnStrategy();
 };
 
 
@@ -288,7 +346,7 @@ LeftStrategy.prototype.name = 'LeftStrategy';
 
 LeftStrategy.prototype.do = function () {
     game.inputManager.emit('move', LEFT);
-    returnStrategy();
+    runner.returnStrategy();
 };
 
 LeftStrategy.prototype.onSame = function () {
@@ -308,7 +366,7 @@ DownUpStrategy.prototype.name = 'DownUpStrategy';
 
 DownUpStrategy.prototype.do = function () {
     if (this.cur == this.mvts.length) {
-        returnStrategy();
+        runner.returnStrategy();
         return;
     }
     game.inputManager.emit('move', this.mvts[this.cur]);
